b4ffe7959fb31f3f022b2e9887655c105a927908
Manon Charron manoncharron8@gmail.com <manoncharron8@gmail.com>
2018-11-23 11:28:31 +0100

    photo uploader

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -1,5 +1,13 @@
 class PhotoUploader < CarrierWave::Uploader::Base
     include Cloudinary::CarrierWave
+
+    process eager: true  # Force version generation at upload time.
+
+    process convert: 'jpg'
+
+    version :thumbnail do
+      resize_to_fit 256, 256
+    end
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
